5b4c8e0347c8b69d2ce8ac5f78e3bdf3777184e5
Anders Launer Bæk <anbae12@student.sdu.dk>
2014-10-24 17:24:02 +0200

    main

---
+++ Code/Navigation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Navigation
+//
+//  Created by Anders Launer Bæk on 24/10/14.
+//  Copyright (c) 2014 Anders Launer Bæk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
