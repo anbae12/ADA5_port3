6bd0cbcd827e97eccaf192e4e513d79ee872296b
Anders Launer Bæk <anbae12@student.sdu.dk>
2014-10-27 09:10:42 +0100

    operator overload. Næsten implementeret

--- Code/Navigation/main.cpp
+++ Code/Navigation/main.cpp
@@ -10,12 +10,27 @@
 #include "FileHandle.h"
 #include "Graph.h"
 #include <fstream>
+#include <queue>
+#include <vector>
+
+
+struct Comp{
+    bool operator()(const VertexTemp& a,const VertexTemp& b){
+        return b.cost<a.cost;
+    }
+};
+
 
 int main(int argc, const char * argv[]) {
+    std::priority_queue< VertexTemp, std::vector<VertexTemp>, Comp> pq;
+    
 	clock_timer timerrecord;
 	timerrecord.start_timer();
 
-	FileHandle filehandle("../../data.raw");
+	/*
+    //FileHandle filehandle("../../data.raw");
+    FileHandle filehandle("/Users/anderslaunerbaek/Documents/data.raw");
+    
 
 	Graph graph;
 	filehandle.doParse(graph);
@@ -29,12 +44,33 @@ int main(int argc, const char * argv[]) {
 	myfile2.open ("from-graph.txt");
 	myfile2 << graph.printFrom("Nysted");
 	myfile2.close();
-	/*
+	
 	for(auto i = vertices.begin(); i != vertices.end(); ++i){
 		std::cout <<  "from: " << (*i).from << "\tcity: " << (*i).to << "\tcost: " << (*i).cost << std::endl;
 		graph.addVertex(i->from);
 	}
 	 */
+    
+    
+    
+    
+    
+    
+    pq.push(VertexTemp("Middelfart", "Odense1",200));
+    pq.push(VertexTemp("Middelfart", "Odense2",210));
+    pq.push(VertexTemp("Middelfart", "Odense3",22));
+    
+    
+    VertexTemp b = pq.top();
+    pq.pop();
+    std::cout<<b.from<<" "<<b.to<<" "<<b.cost<<std::endl;
+
+    VertexTemp bb = pq.top();
+    pq.pop();
+    std::cout<<bb.from<<" "<<bb.to<<" "<<bb.cost<<std::endl;
+    
+    
+    
 	std::cout << "Hello, World!\n";
 	timerrecord.stop_timer();
 
