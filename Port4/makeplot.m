b73051f1bfcf0a52ddf85f39dd13c3f187faedf4
Anders Launer Bæk <anbae12@student.sdu.dk>
2014-11-19 11:06:34 +0100

    A.txt - O(N^2) B.txt - O(N Log N)

--- Port4/makeplot.m
+++ Port4/makeplot.m
@@ -3,23 +3,17 @@
 clear;
 clc;
 %% A)
-maxNo=30000; % Have to checked...
-
-load example.txt;
-x = example(:,2);
-y = example(:,1);
-
-n = (1:maxNo);
+load A.txt;
+x = A(:,2);
+y = A(:,1);
+n = (1:y(length(y)));
 yy=power(n,2);
-
 figure(1)
 subplot(2,1,1)
 plot(n,yy)   %plot 1
 xlabel('Time [ms]');
 ylabel('Elements N');
 title('O(N^2)');
-
-
 subplot(2,1,2)
 plot(y,x,'o');
 xlabel('Time [ms]');
@@ -28,26 +22,20 @@ title('Time as function of elements');
 saveas(gca,'report/graphics/test1', 'epsc')
 
 % B)
-load datanum.txt;
-x1 = datanum(:,2);
-y1 = datanum(:,1);
-
-n1 = (1:maxNo);
+load B.txt;
+x1 = B(:,2);
+y1 = B(:,1);
+n1 = (1:y1(length(y1)));
 yy1=times(n1,log10(n1));
-
 figure(2)
 subplot(2,1,1)
 plot(n1,yy1)
 xlabel('Time [ms]');
 ylabel('Elements N');
 title('O(N log(N))');
-
-
 subplot(2,1,2)
 plot(y1,x1,'o');
 xlabel('Time [ms]');
 ylabel('Elements N');
 title('Time as function of elements');
 saveas(gca,'report/graphics/test2', 'epsc')
-
-
